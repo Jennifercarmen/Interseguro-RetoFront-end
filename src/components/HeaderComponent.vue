b0efd08b61393ef5d8038160bcd130ae9bfcb0fc
JenniferCarmen <jennifercarmens@gmail.com>
2018-05-26 03:43:07 -0500

    añadiendo vuetify, componente header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Interseguro</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
